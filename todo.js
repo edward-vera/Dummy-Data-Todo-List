89aa4894ea50d598f6ef455545852ed348fe08a1
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-06-26 18:06:14 -0500

    finished

--- todo.js
+++ todo.js
@@ -1,67 +1,81 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-    let Todolist = document.getElementById("todo-list")
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }
-]
-
-    console.log(arrayOfTodos[0].userId) // => 14
-    console.log(arrayOfTodos[1].userId) // => 20
-    console.log(arrayOfTodos[0].id) // => 1
-    console.log(arrayOfTodos[1].id) // => 2
-    console.log(arrayOfTodos[0].title) // => delectus aut autem
-    console.log(arrayOfTodos[1].title) // => delectus aut autem
-    console.log(arrayOfTodos[0].completed) // => false
-    console.log(arrayOfTodos[1].completed) // => false
-    
-    const fetchTodos = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then( (response) => response.json())
-        .then( (json) => arrayOfTodos = json)
-    }
-    
-    const logTodos = () => {
-        console.log(arrayOfTodos)
-    }
-    
-    // const populateTodos = () => {
-    //         arrayOfTodos.forEach (todo => {
-    //         let newListItem = document.createElement("li")
-    //         let text = document.createTextNode(todo.userId)
-    //         newListItem.appendChild(text)
-    //         Todolist.appendChild(newListItem)
-    //         // console.log(arrayOfTodos[0].userId)
-    //     })
-
-    // }
-
-    const grabId = () => {
-            arrayOfTodos.forEach (todo => {
-            let newListItem = document.createElement('li')
-            let text = document.createTextNode(todo.userId)
-            newListItem.appendChild(text)
-            Todolist.appendChild(newListItem)
-            console.log(arrayOfTodos[0].userId)
-        })        
-    }
-
-    function customReset() {
-        document.getElementById("todo-list").innerHTML = ("");
-     }
-
-    // const result = userId.filter((userId) => userId.number < 10);
-        // console.log('onlyTitles', onlyTitles)
-
-
+        let arrayOfTodos;
+        let listItems;
+        let lis;
+        let userIDList;
+        let idInput;
+        let array;
+        
+        const fetchTodos = () => {
+          fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((response) => response.json())
+            .then((data) => (arrayOfTodos = data));
+        };
+        
+        const logTodos = () => {
+          console.log(arrayOfTodos);
+        };
+        const populateTodos = () => {
+          let orderedList = document.getElementById("todo-list");
+          arrayOfTodos.forEach((todo) => {
+            listItems = document.createElement("li");
+            const titleToDo = todo.title;
+            listItems.innerHTML = `${titleToDo}`;
+            orderedList.appendChild(listItems);
+          });
+        };
+        
+        const userID = (id) => {
+          lis = document.querySelectorAll("li");
+          lis.forEach((li) => li.parentNode.removeChild(li));
+          idInput = +document.querySelector("#quantity").value;
+          // console.log(idInput);
+          let orderedList = document.getElementById("todo-list");
+          userIDList = arrayOfTodos.filter((todo) => todo.userId === idInput);
+          // console.log(userIDList);
+          // console.log(typeof idInput);
+          userIDList.forEach((todo) => {
+            listItems = document.createElement("li");
+            const titleToDo = todo.title;
+            // console.log(titleToDo);
+            listItems.innerHTML = `${titleToDo}`;
+            orderedList.appendChild(listItems);
+          });
+        };
+        
+        const completedTodos = () => {
+          lis = document.querySelectorAll("li");
+          lis.forEach((li) => li.parentNode.removeChild(li));
+          let orderedList = document.getElementById("todo-list");
+          if (idInput === undefined) {
+            array = arrayOfTodos;
+          } else array = userIDList;
+          // console.log(array);
+          const completedTasks = array.filter((todo) => todo.completed === true);
+          // console.log(completedTasks);
+          completedTasks.forEach((todo) => {
+            listItems = document.createElement("li");
+            const titleToDo = todo.title;
+            // console.log(titleToDo);
+            listItems.innerHTML = `${titleToDo}`;
+            orderedList.appendChild(listItems);
+          });
+        };
+        const incompleteTodos = () => {
+          lis = document.querySelectorAll("li");
+          lis.forEach((li) => li.parentNode.removeChild(li));
+          let orderedList = document.getElementById("todo-list");
+          if (idInput === undefined) {
+            array = arrayOfTodos;
+          } else array = userIDList;
+          // console.log(array);
+        
+          const incompleteTasks = array.filter((todo) => todo.completed === false);
+          incompleteTasks.forEach((todo) => {
+            listItems = document.createElement("li");
+            const titleToDo = todo.title;
+            // console.log(titleToDo);
+            listItems.innerHTML = `${titleToDo}`;
+            orderedList.appendChild(listItems);
+          });
+        };
 
